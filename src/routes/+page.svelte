acdffc2e5cc11f4104a1254f62654c27777efae7
Allen Pike <allen@forestwalk.ai>
2025-03-28 15:54:59 -0700

    Intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,12 +37,34 @@
 					E
 				</div>
 			</div>
+
+			<h2 class="text-2xl font-bold">Introduction</h2>
 			<p class="py-6">
-				The Nutri-Score system helps consumers make healthier food choices by providing an
-				at-a-glance rating from A (healthiest) to E (least healthy). Our calculator lets you explore
-				and compare nutritional values of various foods.
+				The Nutri-Score is a packaging label first adopted by France, and recommended by other EU
+				countries and the WHO.
 			</p>
+			<p>
+				The idea is that you can compare to packaged foods (for example, boxes of cereal) and get a
+				rough sense of which ones are less unhealthy.
+			</p>
+			<h2 class="mt-8 text-2xl font-bold">The Calculator</h2>
+			<div class="py-6">
+				We don't have labelling like this yet in the US or Canada, so here is a quick calculator for
+				snack foods.
+				<ul>
+					<li>Some cereals and snack bars are pre-loaded.</li>
+					<li>If you add a food, it will be stored in your browser.</li>
+				</ul>
+			</div>
 			<a href="/calculator" class="btn btn-primary"> Open Calculator </a>
 		</div>
 	</div>
 </div>
+
+<footer class="fixed bottom-0 w-full pb-2 text-center">
+	<p class="text-sm">
+		Made with ❤️ by <a href="https://allenpike.com/" class="text-primary hover:underline"
+			>Allen Pike</a
+		>
+	</p>
+</footer>
