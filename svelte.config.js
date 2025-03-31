1263052bd350f956de1283972caa2a336be01b52
Allen Pike <allen@forestwalk.ai>
2025-03-30 17:58:37 -0700

    Try 3

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		// Use absolute paths for assets
+		paths: {
+			base: ''
+		},
+		// This forces SvelteKit to use absolute URLs for assets
+		trailingSlash: 'never',
+
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
